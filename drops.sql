1280528538c2104455974f2d610b40ec569f7680
DanielDiggsWHS <>
2021-05-13 14:48:32 +0000

    Initial commit

---
+++ drops.sql
@@ -0,0 +1,22 @@
+/*
+--------------------------------------------------------------------
+© 2017 sqlservertutorial_net All Rights Reserved
+--------------------------------------------------------------------
+Name   : BikeStores
+Link   : http://www_sqlservertutorial_net/load-sample-database/
+Version: 1_0
+--------------------------------------------------------------------
+*/
+
+-- drop tables
+DROP TABLE IF EXISTS sales_order_items;
+DROP TABLE IF EXISTS sales_orders;
+DROP TABLE IF EXISTS production_stocks;
+DROP TABLE IF EXISTS production_products;
+DROP TABLE IF EXISTS production_categories;
+DROP TABLE IF EXISTS production_brands;
+DROP TABLE IF EXISTS sales_customers;
+DROP TABLE IF EXISTS sales_staffs;
+DROP TABLE IF EXISTS sales_stores;
+
+
---
+++ main.sql
@@ -0,0 +1,7 @@
+.headers on
+
+.read drops.sql
+.read create.sql
+.read load.sql
+
+.dump shopping_database.db
